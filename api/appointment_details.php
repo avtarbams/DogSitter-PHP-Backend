476dffd0df6355cffe130875f229bca2c809f244
richiescarvalho <richie.carvalho0910@gmail.com>
2018-11-02 14:28:59 -0400

    Added handling for single quote

--- api/appointment_details.php
+++ api/appointment_details.php
@@ -12,6 +12,8 @@ require_once (CLASS_FILE_PATH."/class_appointment.php");
 $REQUEST = $_REQUEST;
 switch ($REQUEST['api_name']) {
     case "search_pet_sitter":
-
+            $appt_det = new class_appointment();
+            $response = $appt_det->get_available_pet_sitter($REQUEST);
+            echo json_encode($response);
         break;
 }
--- functionDir/class_appointment.php
+++ functionDir/class_appointment.php
@@ -23,22 +23,43 @@ class class_appointment
 
     public function get_available_pet_sitter($search_details){
 
-        $select_pet_sitter = "SELECT psd.user_details_userid AS userid FROM pet_sitter__details WHERE ps_details_id NOT IN(
-                                SELECT pet_sitter_details_ps_details_id FROM ".DB_NAME.".appointment_booking_details WHERE 
-                                abd.appointment_date = '".$search_details['filter_date']."')";
+        $filter_date = date("Y-m-d 00:00:00",time($search_details['filter_date']));
+        $query_booked = "SELECT abd.pet_sitter_details_ps_details_id FROM ".DB_NAME.".appointment_booking_details abd WHERE 
+                                abd.appointment_date = '".$filter_date."'";
+        $res_booked = $this->db_connection->query($query_booked);
+        if ($this->db_connection->num_of_rows($res_booked)>0){
+            $result = $this->db_connection->fetch_data($res_booked);
+            $where_query = "WHERE psd.ps_details_id NOT IN('".implode("','",$result)."')";
+        }
+        else{
+            $where_query = "";
+        }
+        $select_pet_sitter = "SELECT psd.user_details_userid AS userid FROM ".DB_NAME.".pet_sitter_details psd ".$where_query;
 
         $res = $this->db_connection->query($select_pet_sitter,1);
+        $pet_sitter_details = [];
+
         if($this->db_connection->num_of_rows($res)){
             $rows = $this->db_connection->fetch_data($res);
             $login_det = new class_login();
             $common_det = new class_common();
+
             foreach($rows as $key=>$userid){
                 $userid_det['user_id'] = $userid;
                 $sitter_user_details = $login_det->get_user_details($userid_det);
 
                 $sitter_user_details['latest_feedback'] = $common_det->get_user_feedback($userid_det,1);
-
+                $pet_sitter_details[] = $sitter_user_details;
             }
+            $response['status'] = SUCCESS;
+            $response['msg'] = SUCCESS_MSG;
+            $response['data'] = $pet_sitter_details;
+            return $response;
+        }
+        else{
+            $response['status'] = FAILED;
+            $response['msg'] = ERROR_MSG;
+            return $response;
         }
     }
 }
